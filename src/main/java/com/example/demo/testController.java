1af0b68399ad95c72216cea8f44a6947389469dd
edoll1843 <edoll1843@naver.com>
2023-02-03 16:42:34 +0900

    git test commit

--- src/main/java/com/example/demo/testController.java
+++ src/main/java/com/example/demo/testController.java
@@ -6,11 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@Controller //@RestController
 public class testController {
     
     @GetMapping("/")
-    public String test() {
+    public String indexPage() { //welcomepage interccetptor
         return "index.html";
     }
+
+    @GetMapping("/blog")
+    public String blogPage() { //mapping bolg page
+        return "blog.html";
+    }
+
+ 
+
 }
